c6d8739351caee5d62ddf0111b92b7a8354ccc85
Ali Rezaei <eternalsurge@outlook.com>
2018-06-08 14:30:10 +0430

    Major Changes

--- Accord/App.xaml.cs
+++ Accord/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Accord.Models;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,6 @@ namespace Accord
     /// </summary>
     public partial class App : Application
     {
+        public static User _user = new User();
     }
 }
