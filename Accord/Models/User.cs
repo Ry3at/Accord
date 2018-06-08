0e06a5c85c57d0bb952a80d42f4954cc0eb22aa2
chiefmb <35791547+chiefmb@users.noreply.github.com>
2018-06-08 14:58:05 +0430

    UserProfile Builded

--- Accord/Models/User.cs
+++ Accord/Models/User.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 
 namespace Accord.Models
 {
@@ -15,6 +17,7 @@ namespace Accord.Models
         private string email;
         private string phoneNumber;
         private DateTime registrationDate;
+        private BitmapImage _profileImage;
 
         public int UserID { get => userID; set => userID = value; }
         public string UserName { get => userName; set => userName = value; }
@@ -23,5 +26,11 @@ namespace Accord.Models
         public string Email { get => email; set => email = value; }
         public string PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
         public DateTime RegistrationDate { get => registrationDate; set => registrationDate = value; }
+
+        public BitmapImage ProfileImage
+        {
+            get => _profileImage;
+            set => _profileImage = value;
+        }
     }
 }
